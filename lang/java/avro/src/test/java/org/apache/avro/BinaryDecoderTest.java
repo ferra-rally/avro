d7d233bea17f4e34f1aa1597f97f72b3a7b8c15a
ferra-rally <56554604+ferra-rally@users.noreply.github.com>
2021-06-26 10:32:44 +0200

    Added first part of the test

--- lang/java/avro/src/test/java/org/apache/avro/BinaryDecoderTest.java
+++ lang/java/avro/src/test/java/org/apache/avro/BinaryDecoderTest.java
@@ -1,11 +1,47 @@
 package org.apache.avro;
 
+import org.apache.avro.file.DataFileWriter;
+import org.apache.avro.generic.GenericData;
+import org.apache.avro.generic.GenericRecord;
+import org.apache.avro.io.DatumWriter;
+import org.apache.avro.specific.SpecificDatumWriter;
 import org.junit.Before;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
 
 public class BinaryDecoderTest {
+  private Schema schema;
 
   @Before
   public void config() {
-    //Schema schema = SchemaBuilder.record("Test");
+    schema = SchemaBuilder.record("User")
+      .namespace("test.avro")
+      .fields()
+      .endRecord();
+
   }
+
+  @Test
+  public void test() throws IOException {
+    /*
+    User user1 = new User();
+    user1.setName("Alyssa");
+    user1.setNumber(256);
+
+    User user2 = new User("Ben", 7, "red");
+
+    DatumWriter<User> userDatumWriter = new SpecificDatumWriter<>(User.class);
+    DataFileWriter<User> dataFileWriter = new DataFileWriter<>(userDatumWriter);
+    dataFileWriter.create(schema, new File("users.avro"));
+
+    dataFileWriter.append(user1);
+    dataFileWriter.append(user2);
+
+    */
+
+
+  }
+
 }
