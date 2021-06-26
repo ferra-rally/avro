02cd5e79ed47d7a3e43fbbef609a7ddca1484b51
ferra-rally <danieleferrarelli@gmail.com>
2021-06-26 09:41:45 +0200

    Added first tests

---
+++ lang/java/avro/src/test/java/org/apache/avro/BinaryDecoderTest.java
@@ -0,0 +1,11 @@
+package org.apache.avro;
+
+import org.junit.Before;
+
+public class BinaryDecoderTest {
+
+  @Before
+  public void config() {
+    //Schema schema = SchemaBuilder.record("Test");
+  }
+}
---
+++ lang/java/avro/src/test/java/org/apache/avro/GenericDataTest.java
@@ -0,0 +1,12 @@
+package org.apache.avro;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class GenericDataTest {
+
+  @Test
+  public void test() {
+    Assert.assertTrue(true);
+  }
+}
