da06568cb96757a4d8da55912377fcdce92e1ca3
ferra-rally <56554604+ferra-rally@users.noreply.github.com>
2021-07-03 15:26:33 +0200

    Edited SchemaTest

--- lang/java/avro/src/test/java/org/apache/avro/SchemaTest.java
+++ lang/java/avro/src/test/java/org/apache/avro/SchemaTest.java
@@ -3,7 +3,9 @@ package org.apache.avro;
 import com.fasterxml.jackson.core.JsonEncoding;
 import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.avro.example.SampleClass;
 import org.apache.avro.util.ByteBufferInputStream;
 import org.apache.avro.util.ByteBufferOutputStream;
@@ -23,20 +25,37 @@ public class SchemaTest {
   private List<String> enumFields;
   private boolean isError;
   private List<Schema.Field> fields;
+  private String doc;
+  private String nameSpace;
 
-  public SchemaTest(String name, List<String> aliasList, List<String> enumFields, boolean isError, List<Schema.Field> fields) {
+  /*
+  public SchemaTest(String name, List<String> aliasList, List<String> enumFields, boolean isError, List<Schema.Field> fields, String doc, String nameSpace) {
     this.name = name;
     this.aliasList = aliasList;
     this.enumFields = enumFields;
     this.isError = isError;
     this.fields = fields;
+    this.doc = doc;
+    this.nameSpace = nameSpace;
+  }*/
+
+  public SchemaTest(String name, List<String> aliasList, boolean isError, List<Schema.Field> fields, String doc, String nameSpace) {
+    this.name = name;
+    this.aliasList = aliasList;
+    this.isError = isError;
+    this.fields = fields;
+    this.doc = doc;
+    this.nameSpace = nameSpace;
   }
 
   @Parameterized.Parameters
   public static Collection<Object[]> getTestParameters() {
     return Arrays.asList(new Object[][]{
+      /*
       {"test_name", new ArrayList<String>() {{add("alias1"); add("alias2");}}, new ArrayList<String>() {{  add("A"); add("B");}}, false,
-      new ArrayList<Schema.Field>() {{add(new Schema.Field("a", SchemaBuilder.builder().intType(), "", 2));}}}
+      new ArrayList<Schema.Field>() {{add(new Schema.Field("a", SchemaBuilder.builder().intType(), "", 2));}}, "doc", "test.avro"}*/
+      {"test_name", new ArrayList<String>() {{add("alias1"); add("alias2");}}, false,
+        new ArrayList<Schema.Field>() {{add(new Schema.Field("a", SchemaBuilder.builder().intType(), "", 2));}}, "doc", "test.avro"}
     });
   }
 
@@ -45,35 +64,45 @@ public class SchemaTest {
 
   }
 
+  /*
   @Test
   public void schemaEnumTest() {
 
-    Schema schema = Schema.createEnum(name, "", "", enumFields);
+    Schema schema = Schema.createEnum(name, doc, nameSpace, enumFields);
 
     Assert.assertEquals(enumFields, schema.getEnumSymbols());
-  }
+  }*/
 
   @Test
-  public void aliasTest() {
-    Schema schema =  Schema.createEnum(name, "", "", enumFields);
+  public void aliasTest() throws JsonProcessingException {
+    Schema schema = Schema.createRecord(name, doc, nameSpace, isError);
 
     for(String alias : aliasList) {
       schema.addAlias(alias);
     }
 
+    /*
     Set<String> aliases = schema.getAliases();
 
     for(String name : aliases) {
       boolean contains = aliasList.contains(name);
       Assert.assertTrue(contains);
+    }*/
+
+    //Check if correctly created in schema
+    String schemaJson = schema.toString();
+    ObjectMapper mapper = new ObjectMapper();
+    JsonNode actualObj = mapper.readTree(schemaJson);
+
+    JsonNode aliasesJson = actualObj.get("aliases");
+    for(int i = 0; i < aliasesJson.size(); i++) {
+      Assert.assertEquals(aliasList.get(i), aliasesJson.get(i).asText());
     }
   }
 
   @Test
-  public void schemaCreateRecordTest() {
-    List<Schema.Field> fields = new ArrayList<>();
-
-    Schema schema = Schema.createRecord(name, "", "", isError);
+  public void schemaCreateRecordTest() throws JsonProcessingException {
+    Schema schema = Schema.createRecord(name, doc, nameSpace, isError);
 
     schema.setFields(fields);
 
@@ -82,5 +111,16 @@ public class SchemaTest {
 
       Assert.assertEquals(field, out);
     }
+
+    //Check if correctly created in schema
+    /*
+    String schemaJson = schema.toString();
+    ObjectMapper mapper = new ObjectMapper();
+    JsonNode actualObj = mapper.readTree(schemaJson);
+    JsonNode fieldJson = actualObj.get("fields");
+
+    for(int i = 0; i < fields.size(); i++) {
+      Assert.assertEquals(fields.get(i), fieldJson.get(i));
+    }*/
   }
 }
---
+++ lang/java/avro/src/test/java/org/apache/avro/SchemaUnionTest.java
@@ -0,0 +1,34 @@
+package org.apache.avro;
+
+import org.apache.avro.example.SampleClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+@RunWith(value = Parameterized.class)
+public class SchemaUnionTest {
+  private List<Schema> unionSchemes;
+
+  @Parameterized.Parameters
+  public static Collection<Object[]> getTestParameters() {
+    return Arrays.asList(new Object[][]{
+      {new ArrayList<Schema>() {{add(SampleClass.getClassSchema());}}}
+    });
+  }
+
+  public SchemaUnionTest(List<Schema> unionSchemes) {
+    this.unionSchemes = unionSchemes;
+  }
+
+  @Test
+  public void test() {
+    Schema schema = Schema.createUnion(unionSchemes);
+
+    System.out.println(schema);
+  }
+}
